713fb5f03bbc63c8a181343760c212425276bb26
muhmdajeer <muhdajeer@gmail.com>
2022-11-30 11:31:26 +0530

    index changed

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express()
 const logger = require('morgan')
 const dotenv = require('dotenv').config()
 const cors = require('cors')
+// const path = require('path')
+// const fs = require('fs')
 
 const { errorHandler } = require('./Middlewares/errorHandler')
 const db = require('./config/connection')
 const { connection } = require('./config/connection')
+
+// var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
+
+
 
 const userRouter = require('./router/user')
 const jobsRouter = require('./router/jobs')
@@ -17,6 +23,7 @@ const PORT = process.env.PORT || 5000
 connection()
 
 app.disable('etag')
+// app.use(logger('combined',{stream : accessLogStream}))
 app.use(cors())
 app.use(logger('dev'))
 app.use(express.json({ limit: '50mb' }))
